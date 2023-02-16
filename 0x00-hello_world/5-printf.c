7387a0f963b47c7eaa7dc67278b6151c10e9fa3f
ImmaculeeNamahirwe <i.namahirwe@alustudent.com>
2023-02-16 13:52:35 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main -with proper grammar, but the outcome is a piece of art
- * Return: 0
+ * main -A program thet plintsf
+ * Return: 0 (success)
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
+}
 
