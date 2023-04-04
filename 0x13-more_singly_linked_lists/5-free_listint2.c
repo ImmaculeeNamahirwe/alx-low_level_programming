c7be2e0e236f364f63a33bec1b852aedf2598106
ImmaculeeNamahirwe <i.namahirwe@alustudent.com>
2023-04-04 15:02:10 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+*free_listint2 - sets a linked list free
+*@head: pointer at the top of the list
+*
+*Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *cursor;
+listint_t **temp = head;
+
+if (temp != NULL)
+{
+while (*head != NULL)
+{
+cursor = *head;
+free(cursor);
+*head = (*head)->next;
+}
+
+*temp = NULL;
+
+}
+}
